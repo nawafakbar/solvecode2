aa7fdc1dd2fdae9a0e66ba93681314822299f781
nawafakbar <nwfakbart@gmail.com>
2025-12-07 16:37:26 +0700

    href

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,35 +16,36 @@ const poppins = localFont({
 })
 
 const items = [
-    {
-      label: "About",
-      bgColor: "#0D0716",
-      textColor: "#fff",
-      links: [
-        { label: "Company", ariaLabel: "About Company" },
-        { label: "Careers", ariaLabel: "About Careers" }
-      ]
-    },
-    {
-      label: "Projects", 
-      bgColor: "#170D27",
-      textColor: "#fff",
-      links: [
-        { label: "Featured", ariaLabel: "Featured Projects" },
-        { label: "Case Studies", ariaLabel: "Project Case Studies" }
-      ]
-    },
-    {
-      label: "Contact",
-      bgColor: "#271E37", 
-      textColor: "#fff",
-      links: [
-        { label: "Email", ariaLabel: "Email us" },
-        { label: "Twitter", ariaLabel: "Twitter" },
-        { label: "LinkedIn", ariaLabel: "LinkedIn" }
-      ]
-    }
-  ];
+  {
+    label: "About",
+    bgColor: "#0D0716",
+    textColor: "#fff",
+    links: [
+      { label: "Company", ariaLabel: "About Company", href: "/company" },
+      { label: "Careers", ariaLabel: "About Careers", href: "/careers" }
+    ]
+  },
+  {
+    label: "Projects", 
+    bgColor: "#170D27",
+    textColor: "#fff",
+    links: [
+      { label: "Featured", ariaLabel: "Featured Projects", href: "/projects/featured" },
+      { label: "Case Studies", ariaLabel: "Project Case Studies", href: "/projects/case-studies" }
+    ]
+  },
+  {
+    label: "Contact",
+    bgColor: "#271E37", 
+    textColor: "#fff",
+    links: [
+      { label: "Email", ariaLabel: "Email us", href: "/contact" },
+      { label: "Twitter", ariaLabel: "Twitter", href: "https://twitter.com" },
+      { label: "LinkedIn", ariaLabel: "LinkedIn", href: "https://linkedin.com" }
+    ]
+  }
+];
+
 
 export const metadata: Metadata = {
   title: "SolveCode.id",
